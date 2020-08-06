f714f94a21127922818df3decb7874ef80fb59bb
xinyi0526 <xinyi.luvand620@gmail.com>
2020-08-06 20:08:20 +0800

    Enhancement Submission

--- app/src/main/java/com/myapplicationdev/android/p12_mydatabook/AboutUsActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p12_mydatabook/AboutUsActivity.java
@@ -3,11 +3,16 @@ package com.myapplicationdev.android.p12_mydatabook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class AboutUsActivity extends AppCompatActivity {
 
-    TextView tvAbout;
+    TextView tvAbout,tvAuthor;
+    ImageView iv;
+    Boolean success;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,12 @@ public class AboutUsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about_us);
 
         tvAbout = findViewById(R.id.tvAbout);
+        tvAuthor = findViewById(R.id.tvAuthor);
+        iv = findViewById(R.id.ivAbout);
+
+        tvAuthor.setText("Created by : Ah Chong, Peter, Mary");
+        String url = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
+        Picasso.with(this).load(url).placeholder(R.drawable.ajax_loader).error(R.drawable.error).into(iv);
 
         tvAbout.setText("C347 Android Programming || \n Republic Polytechnic");
     }
